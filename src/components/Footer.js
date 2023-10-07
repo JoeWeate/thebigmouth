833a31d88ff2754c05e1f18e2c02f85722c3f037
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-10-07 06:10:02 +0100

    added fonts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,44 @@
 import React from "react";
 import { Container, Grid, Typography, Box, CssBaseline } from "@mui/material";
-import FooterLogo from "..FooterLogo.png";
+import logofooter from "../assets/TheBigMouth.png";
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: "#E6007E", height: "280px" }}>
+    <footer
+      style={{
+        backgroundColor: "#E6007E",
+        width: "100%",
+        position: "absolute",
+        top: "5690px",
+        left: "0",
+        height: "279.72px",
+      }}
+    >
       <CssBaseline />
       <Container maxWidth="lg" style={{ height: "100%" }}>
         <Grid container alignItems="center" style={{ height: "100%" }}>
           <Grid item xs={12} md={4}>
-            {/* Logo */}
             <Box
               display="flex"
               alignItems="center"
               justifyContent="center"
               height="100%"
             >
-              <img src="FooterLogo.png" alt="Logo" width="100" height="100" />
+              <img
+                className="logo-image"
+                src={logofooter}
+                alt="Logo"
+                width="237"
+                height="273"
+                style={{
+                  background:
+                    "var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box",
+                  opacity: "1",
+                }}
+              />
             </Box>
           </Grid>
           <Grid item xs={12} md={8}>
-            {/* Titles */}
             <Box
               display="flex"
               alignItems="center"
@@ -28,16 +46,40 @@ const Footer = () => {
               height="100%"
               flexDirection="row"
             >
-              <Typography variant="h6" style={{ color: "white" }}>
+              <Typography
+                variant="h6"
+                style={{
+                  fontFamily: "Core Sans D 45 Medium W01 Rg",
+                  color: "#E0E0E0",
+                }}
+              >
                 Back to The Big Mouth
               </Typography>
-              <Typography variant="h6" style={{ color: "white" }}>
+              <Typography
+                variant="h6"
+                style={{
+                  fontFamily: "Core Sans D 45 Medium W01 Rg",
+                  color: "#E0E0E0",
+                }}
+              >
                 Become a member
               </Typography>
-              <Typography variant="h6" style={{ color: "white" }}>
+              <Typography
+                variant="h6"
+                style={{
+                  fontFamily: "Core Sans D 45 Medium W01 Rg",
+                  color: "#E0E0E0",
+                }}
+              >
                 Contact
               </Typography>
-              <Typography variant="h6" style={{ color: "white" }}>
+              <Typography
+                variant="h6"
+                style={{
+                  fontFamily: "Core Sans D 45 Medium W01 Rg",
+                  color: "#E0E0E0",
+                }}
+              >
                 Talent Enquiry
               </Typography>
             </Box>
