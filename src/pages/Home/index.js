f0209f87f9e818307cbc3e42e52bd8f2399db9e8
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-10-07 15:00:38 +0100

    done with side by side component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,19 @@
 import React, { Grid, Typography } from '@mui/material'
 import { TitleComponent } from '../../components/TitleComponent'
+import SideBySide from './SideBySide'
 
 export default function Home() {
   return (
-    <Grid container style={{margin: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: 100}}>
+    <>
+    <Grid container style={{ margin: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: 100 }}>
+      
       <TitleComponent
         title="TheBigMouth"
         description=""
         />
-        <Typography>This is our home page</Typography>
-    </Grid>
+      <Typography>This is our home page</Typography>
+      <SideBySide />
+      </Grid>
+      </>
   )
 }
