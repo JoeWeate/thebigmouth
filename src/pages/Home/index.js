7cf03151b25605aabd0b7343424099c384bf806b
AndriusIsin <isin.andrius@gmail.com>
2023-10-11 22:19:53 +0100

    done video banner

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,22 @@
-import React, { Grid, Typography } from '@mui/material'
-import { TitleComponent } from '../../components/TitleComponent'
-
+import React, { Grid, Typography } from "@mui/material";
+import { TitleComponent } from "../../components/TitleComponent";
+import VideoBanner from "../../components/VideoBanner";
 export default function Home() {
   return (
-    <Grid container style={{margin: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: 100}}>
-      <TitleComponent
-        title="TheBigMouth"
-        description=""
-        />
-        <Typography>This is our home page</Typography>
+    <Grid
+      container
+      style={{
+        margin: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: 100,
+      }}
+    >
+      <TitleComponent title="TheBigMouth" description="" />
+      <Typography>This is our home page</Typography>
+      <VideoBanner />
     </Grid>
-  )
+  );
 }
