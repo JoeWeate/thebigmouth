d1d117c43554d24d504eb5acd7fa97c24953ce25
JoeWeate <joey@saltnprep.com>
2023-10-14 14:21:04 +0100

    hello

--- src/hooks/API/helloworld.js
+++ src/hooks/API/helloworld.js
@@ -4,7 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 const api = configureAxios({});
 
 export const getHelloWorld = (name = 'Joey') => {
-    return api.get(`/world/${name}`, {
+    return api.get(`/world`, {
         name:"Joey"
     }).then((data) => {
         console.log(data.data)
