238ce468afed7590811b9232bec02da8cfe06baa
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-10-18 12:39:46 +0100

    fix the navbar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,14 +5,14 @@ import { TitleComponent } from '../../components/TitleComponent'
 export default function Home() {
   const [message, setMessage] = useState('')
 
-  useEffect(()=>{
-    getHelloWorld().then((msg) => {
-      setMessage(msg)
-  })
-  },[])
+  // useEffect(()=>{
+  //   getHelloWorld().then((msg) => {
+  //     setMessage(msg)
+  // })
+  // },[])
 
   return (
-    <>
+  
     <Grid container style={{ margin: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: 100 }}>
       
       <TitleComponent
@@ -21,17 +21,8 @@ export default function Home() {
         />
         <Typography>This is our home page</Typography>
     </Grid>
+  
   )
 }
 
 
-
-
-
-/*
-S3 Example
-  <img
-    src={`https://${env_config.s3.BUCKET}.s3.amazonaws.com/public/big-mouth.png`}
-    style={{width: 250, height: 250}}
-    />
-*/
