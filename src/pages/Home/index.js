16c6c4def79248bac2c4e83d871ab4c750a688e0
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-10-18 12:57:25 +0100

    merge SideBySide

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Grid, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { TitleComponent } from '../../components/TitleComponent'
+import SideBySide from "../../components/sideBySide/SideBySide"
 
 export default function Home() {
   const [message, setMessage] = useState('')
@@ -13,13 +14,13 @@ export default function Home() {
 
   return (
   
-    <Grid container style={{ margin: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: 100 }}>
+    <Grid container >
       
       <TitleComponent
         title="TheBigMouth"
         description=""
         />
-        <Typography>This is our home page</Typography>
+        <SideBySide/>
     </Grid>
   
   )
