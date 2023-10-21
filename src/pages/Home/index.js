552e414c42325db4b0832a6243c46bef5e2c5c70
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-10-21 11:44:39 +0100

    delete home use-effect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,41 +1,47 @@
-import React, { Grid, Typography } from '@mui/material'
-import { useEffect, useState } from 'react'
-import { TitleComponent } from '../../components/TitleComponent'
-import { getHelloWorld, getHelloWorldByName } from '../../hooks/API/helloworld'
-import env_config from '../../env_config'
-import { useAuth0 } from "@auth0/auth0-react"
+import React, { Grid, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { TitleComponent } from "../../components/TitleComponent";
+import { getHelloWorld, getHelloWorldByName } from "../../hooks/API/helloworld";
+import env_config from "../../env_config";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Home() {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState("");
   const { user, isLoading } = useAuth0();
 
-  useEffect(()=>{
-    getHelloWorld().then((msg) => {
-      setMessage(msg)
-  })
-  },[])
+  // useEffect(()=>{
+  //   getHelloWorld().then((msg) => {
+  //     setMessage(msg)
+  // })
+  // },[])
 
-  useEffect(()=>{
-    if(!isLoading && user){
-        getHelloWorldByName(user.nickname).then((msg) => {
-          setMessage(msg)
-      })
-    }
-  },[isLoading, user])
+  // useEffect(()=>{
+  //   if(!isLoading && user){
+  //       getHelloWorldByName(user.nickname).then((msg) => {
+  //         setMessage(msg)
+  //     })
+  //   }
+  // },[isLoading, user])
 
   return (
-    <Grid container style={{margin: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: 100}}>
-      <TitleComponent
-        title="TheBigMouth"
-        description=""
-        />
-        <Typography>This is our home page</Typography>
-        <Typography variant='h5'>{message}</Typography>
-        <img
-          src={`https://${env_config.s3.BUCKET}.s3.amazonaws.com/public/big-mouth.png`}
-          style={{width: 250, height: 250}}
-          />
+    <Grid
+      container
+      style={{
+        margin: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: 100,
+      }}
+    >
+      <TitleComponent title="TheBigMouth" description="" />
+      <Typography>This is our home page</Typography>
+      <Typography variant="h5">{message}</Typography>
+      <img
+        src={`https://${env_config.s3.BUCKET}.s3.amazonaws.com/public/big-mouth.png`}
+        style={{ width: 250, height: 250 }}
+      />
     </Grid>
-  )
+  );
 }
-
