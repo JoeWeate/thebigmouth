c87f66635420263581df621f48337d209e2b4a8f
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-10-28 02:31:34 +0100

    Updated multimedia-Section\index.js

--- src/components/multimedia-Section/index.js
+++ src/components/multimedia-Section/index.js
@@ -1,6 +1,8 @@
 import configureAxios from '../../hooks/configureAxios'; 
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VideoSection from './VideoSection/VideoSection';
+import Multimedia from '../../pages/Multimedia/index'; // Import the Multimedia component
 import './index.css';
 import env_config from "../../env_config";
 
@@ -21,15 +23,15 @@ function Index(props) {
         console.error('Error fetching data:', error);
       });
   }, []);
-  console.log( multimediaData);
 
- 
   return (
     <div className="container-mult">
       <VideoSection sectionTitle="New to the Bigmouth" multimediaData={multimediaData} />
       <VideoSection sectionTitle="Behind the Scenes" multimediaData={multimediaData} />
       <VideoSection sectionTitle="Short" multimediaData={multimediaData} />
-   
+      <Routes>
+        <Route path='/multimedia/:ID' element={<Multimedia />} />
+      </Routes>
     </div>
   );
 }
