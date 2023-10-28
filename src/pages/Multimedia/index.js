b778c78725bd8598b129070c9510788ebee5d719
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-10-28 03:44:14 +0100

    Updated index.js

--- src/pages/Multimedia/index.js
+++ src/pages/Multimedia/index.js
@@ -7,36 +7,78 @@ import AboutInfo from '../../components/Page4/About/AboutInfo';
 import Information from '../../components/Page4/Information';
 import { getOneMultimedia } from '../../hooks/API/multimedia';
 import styles from './index.module.css';
+import configureAxios from '../../hooks/configureAxios';
+import { colors } from '@mui/material';
+import SeasonComponent from '../../components/SeasonComponent/SeasonComponent';
 
 const Multimedia = () => {
   const params = useParams();
   const { ID } = params;
   const [media, setMedia] = useState({});
-  const [episodes, setEpisodes] = useState({});
   const [isLoadingMedia, setIsLoadingMedia] = useState(true);
+  const [loadingEpisodes, setLoadingEpisodes] = useState(true);
 
   useEffect(() => {
+    const axios = configureAxios();
+  
     setIsLoadingMedia(true);
-    getOneMultimedia(ID)
-      .then((data) => {
-        const { multimedia, episodes } = data || {};
-        setMedia(multimedia); 
-        setEpisodes(episodes);
+    axios.get(`/multimedia`)
+      .then((response) => {
+        const { multimedia } = response.data || {};
+        console.log('Multimedia Data:', multimedia);
+        console.log('Target ID:', ID);
+        
+        const selectedMedia = multimedia.find(item => item.ID.S.toString() === ID.toString());
+        setMedia(selectedMedia);
+      })
+      .catch((error) => {
+        console.log('Error fetching multimedia data:', error);
       })
       .finally(() => setIsLoadingMedia(false));
-  }, []);
+  }, [ID]);
   
-console.log(media.Description?.S);
+  
+  useEffect(() => {
+    const axios = configureAxios();
+
+    if (media?.ID?.S) {
+      axios.get(`/multimedia/${media.ID.S}`)
+        .then((response) => {
+          const { episodes } = response.data || {};
+          setMedia((prevMedia) => ({
+            ...prevMedia,
+            episodes,
+          }));
+        })
+        .catch((error) => {
+          console.log('Error fetching episodes:', error);
+          // Handle error (e.g., show user-friendly error message)
+        })
+        .finally(() => setLoadingEpisodes(false));
+    }
+  }, [media.ID?.S]);
 
   return (
-    <Grid container className={styles.container}>
+    <>
+    <Grid container sx={{backgroundColor: "#2B2B2B"}} className={styles.container}>
       <Grid item xs={12}>
         {!isLoadingMedia && isEmpty(media) && <p>Try another ID! There is no media with ID {ID}</p>}
         {!isLoadingMedia && !isEmpty(media) && <BannerMultimedia src={media.images?.S} alt={media.Name?.S} />}
       </Grid>
       <Grid item xs={12}>
-        <AboutInfo description={media.Description?.S} />
+        <AboutInfo episode={media.Description?.S} />
+        {/* <p >{media.Description?.S}</p> */}
+
       </Grid>
+ 
+      <Grid item xs={12}>
+          {!isLoadingMedia && !isEmpty(media) && (
+            <SeasonComponent
+              episodes={media.episodes}
+            />
+          )}
+        </Grid>
+
       <Grid item xs={12}>
         {!isLoadingMedia && !isEmpty(media) && (
           <Information
@@ -48,6 +90,8 @@ console.log(media.Description?.S);
         )}
       </Grid>
     </Grid>
+
+    </>
   );
 };
 
