7d3c47644a5c3bb18f6d82a0ac97438a3af5346c
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-03 14:29:21 +0000

    Updated css

--- src/components/Page4/Information.js
+++ src/components/Page4/Information.js
@@ -24,7 +24,7 @@ const Information = ({released, rated, regionOfOrigin, originalAudio}) => {
             <div className="info-container">
                 <Typography className="info"
                     sx={
-                        {color: 'black'}
+                        {color: 'White'}
                 }>
                     <p>INFORMATION</p>
                 </Typography>
