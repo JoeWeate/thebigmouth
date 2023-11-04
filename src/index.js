96347734e8fd9c9ab554c5ae6963cafe0370bf6c
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-11-04 10:35:32 +0000

    episode

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Grid, CircularProgress } from '@mui/material'
-import { ThemeProvider } from '@mui/material/styles'
-import { Auth0Provider } from '@auth0/auth0-react';
-import env_config from "./env_config"
-import Home from './pages/Home'
-import Error from './pages/Error'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Grid, CircularProgress } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { Auth0Provider } from "@auth0/auth0-react";
+import env_config from "./env_config";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
 import Multimedia from "./pages/Multimedia";
 
-import Theme from './theme'
-import App from './App'
+import Theme from "./theme";
+import App from "./App";
 
-import Account from './pages/Account/index'
-import PostAuthentication from './pages/PostAuthentication/index'
-import ProtectedRoute from './components/Auth/ProtectedRoute'
-import reportWebVitals from './reportWebVitals'
-import Episode from './pages/Episode';
+import Account from "./pages/Account/index";
+import PostAuthentication from "./pages/PostAuthentication/index";
+import ProtectedRoute from "./components/Auth/ProtectedRoute";
+import reportWebVitals from "./reportWebVitals";
+import Episode from "./pages/Episode";
 
-import './index.css'
-
+import "./index.css";
 
 ReactDOM.render(
   <ThemeProvider theme={Theme}>
@@ -28,25 +27,46 @@ ReactDOM.render(
       clientId={env_config.auth0.clientId}
       authorizationParams={{
         audience: env_config.auth0.apiAudience,
-        redirect_uri: window.location.origin + '/login'
+        redirect_uri: window.location.origin + "/login",
       }}
     >
-      <Suspense fallback={<Grid container style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}><CircularProgress color='primary' /></Grid>}>
+      <Suspense
+        fallback={
+          <Grid
+            container
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100vh",
+            }}
+          >
+            <CircularProgress color="primary" />
+          </Grid>
+        }
+      >
         <BrowserRouter>
           <Routes>
-            <Route exact path='/' element={<App />}>
-              <Route path='' element={<Home />} />
-              <Route path='/multimedia/:ID' element={<Multimedia />} />
-              <Route path='/episode/:id' element={<Episode />} />
-              <Route path='login' element={<PostAuthentication />} />
-              <Route path='profile' element={<ProtectedRoute><Account /></ProtectedRoute>} />
-              <Route path='*' element={<Error />} />
+            <Route exact path="/" element={<App />}>
+              <Route path="" element={<Home />} />
+              <Route path="/multimedia/:ID" element={<Multimedia />} />
+              <Route path="/episode/:id" element={<Episode />} />
+              <Route path="login" element={<PostAuthentication />} />
+              <Route
+                path="profile"
+                element={
+                  <ProtectedRoute>
+                    <Account />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="*" element={<Error />} />
             </Route>
           </Routes>
         </BrowserRouter>
       </Suspense>
     </Auth0Provider>
   </ThemeProvider>,
-  document.getElementById('root')
-)
-reportWebVitals()
+  document.getElementById("root")
+);
+reportWebVitals();
--- src/pages/Episode/index.js
+++ src/pages/Episode/index.js
@@ -3,94 +3,108 @@ import VideoBanner from "../../components/Page4/PlaceholderVideo";
 import Information from "../../components/Page4/Information";
 import { getSingleEpisode } from "../../hooks/API/singleEpisode";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { isEmpty } from "lodash";
 import queryString from "query-string";
 import { Grid } from "@mui/material";
-import configureAxios from "../../hooks/configureAxios"
-
-
-
+import configureAxios from "../../hooks/configureAxios";
 
 function Episode() {
+  const params = useParams();
+  const { id } = params;
+  const [episode, setEpisode] = useState({});
+  const { search } = useLocation();
+  const { user, isLoading } = useAuth0();
+  const [isLoadingEpisode, setIsLoadingEpisode] = useState(true);
+  const [status, setStatus] = useState(true);
+  const parsedQuery = queryString.parse(search, {
+    decodeURIComponent: (str) => str,
+  });
+  const episodeID = parsedQuery.episode_id;
+  const [media, setMedia] = useState({});
+  const [loadingMedia, setLoadingMedia] = useState(true);
+  console.log(id, episodeID);
 
+  // useEffect(() => {
+  //   if (!isLoading && user && episodeID) {
+  //     getSingleEpisode(id, episodeID)
+  //       .then((data) => {
+  //         const { episode } = data || {};
+  //         console.log("data", data);
+  //         setEpisode(episode);
+  //         setIsLoadingEpisode(false);
+  //         if (episode && !episode.rating) {
+  //           setStatus(false);
+  //         }
+  //       })
+  //       .then(() => setIsLoadingEpisode(false));
+  //   }
+  // }, [id, episodeID, user, isLoading]);
 
-    const params = useParams();
-    const { id, episode_id } = params;
-    const [episode, setEpisode] = useState({});
-    const { search } = useLocation();
-    const { user, isLoading } = useAuth0();
-    const [isLoadingEpisode, setIsLoadingEpisode] = useState(false);
-    const [status, setStatus] = useState(true)
-    const parsedQuery = queryString.parse(search, { decodeURIComponent: (str) => str });
-    const episodeID = parsedQuery.episode_id;
-    const [media, setMedia] = useState({});
-    const [loadingMedia, setLoadingMedia] = useState(true);
-    console.log(id, episodeID);
-
-    useEffect(() => {
-      if (!isLoading && user) {
-        setIsLoadingEpisode(true);
-        getSingleEpisode(id, episodeID)
-          .then((data) => {
-            const { episode } = data || {};
-            console.log("data", data);
-            setEpisode(episode);
-            if (!episode.rating) {
-              setStatus(false);
-            }
-          })
-          .then(() => setIsLoadingEpisode(false));
-      }
-    }, [id, episodeID, user, isLoading]);
-
+  // useEffect(() => {
+  //   if (!isLoading && user) {
+  //     setIsLoadingEpisode(true);
+  //     getSingleEpisode(id, episodeID)
+  //       .then((data) => {
+  //         const { episode } = data || {};
+  //         console.log("episode", episode.episode);
+  //         setEpisode(episode);
+  //         if (!episode.rating) {
+  //           setStatus(false);
+  //         }
+  //       })
+  //       .then(() => setIsLoadingEpisode(false));
+  //   }
+  // }, [isLoading, user, id, episodeID]);
 
   useEffect(() => {
     const axios = configureAxios();
     axios
-      .get(`/multimedia/${id}`)
-      .then((response) => {
-        const { multimedia } = response.data || {};
-        setMedia((prevMedia) => ({
-          ...prevMedia,
-          ...multimedia,
-        }));
+      .get(`/episodes/${id}`, { params: { episode_id: episodeID } })
+      .then((data) => {
+        console.log(data.data.episode.description);
+        const newEpisode = data.data;
+        console.log(newEpisode);
+        setEpisode(newEpisode);
       })
       .catch((error) => {
-        console.log("Error fetching media:", error);
+        console.log("Error fetching Episode:", error);
       })
       .finally(() => setLoadingMedia(false));
-  }, [id]);
+  }, [id, episodeID]);
 
+  return (
+    <Grid container sx={{ minHeight: "100vh", width: "100vw", color: "white" }}>
+      <Grid item xs={12}>
+        {!isLoading && !isLoadingEpisode && isEmpty(episode) && (
+          <p>
+            Try another ID! There is no Episode with ID {id} or {episodeID}
+          </p>
+        )}
+        {!isLoadingEpisode && !isEmpty(episode) && (
+          <VideoBanner episode={episode} />
+        )}
+      </Grid>
+      <Grid item xs={12}>
+        {!isLoading && !isEmpty(episode) && (
+          <div>
+            <AboutInfo episode={episode.description} />
 
-    return (
-
-        <Grid container sx={{ minHeight: "100vh", width: "100vw", color: "white" }}>
-            <Grid item xs={12}>
-                {!isLoading && !isLoadingEpisode && isEmpty(episode) && (
-                    <p>Try another ID! There is no Episode with ID {id} or {episodeID}</p>
-                )}
-                {!isLoadingEpisode && !isEmpty(episode) && (
-                    <VideoBanner episode={episode} />
-                )}
-            </Grid>
-            <Grid item xs={12}>
-                {!isLoading && !isEmpty(episode) &&
-                    <div>
-                        <AboutInfo episode={episode.description} />
-                        {/* <Information status={status} /> */}
-                        {!loadingMedia && <Information
-            released={media.released?.N}
-            rated={media.rated?.N}
-            regionOfOrigin={media.region_of_origin?.S}
-            originalAudio={media.original_audio?.S}
-          />}
-                    </div>
-                }
-            </Grid>
-        </Grid>
-    )
+            {/* <Information status={status} /> */}
+            {!loadingMedia && (
+              <Information
+                released={media.released?.N}
+                rated={media.rated?.N}
+                regionOfOrigin={media.region_of_origin?.S}
+                originalAudio={media.original_audio?.S}
+              />
+            )}
+          </div>
+        )}
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Episode;
