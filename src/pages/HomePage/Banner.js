e2f963a0c6196da47bd929b81eda6d0bcd55ee86
Olha Danylevska <odanilevskaya2@gmail.com>
2023-11-07 20:28:09 +0000

    put all styles in line in Banner component

--- src/pages/HomePage/Banner.js
+++ src/pages/HomePage/Banner.js
@@ -1,70 +1,104 @@
-import { Typography, Paper, CardMedia } from "@mui/material";
-import './Banner.css';
+import { Typography, Paper, CardMedia, ThemeProvider, createTheme } from "@mui/material";
 import triangleIcon from "../../assets/images/triangle-icon.svg";
 //import CrossfadeImage from "react-crossfade-image";
 //< CrossfadeImage src="https://thebigmouth-media.s3.eu-west-2.amazonaws.com/public/big-mouth.png" />
 
 
 const defaultImage = "https://thebigmouth-media.s3.eu-west-2.amazonaws.com/public/big-mouth.png";
-const defaultTitle = "The Big Mouth";
+const defaultTitle = "THE BIG MOUTH";
 const defaultBackgroundImageUrl = "https://thebigmouth-media.s3.eu-west-2.amazonaws.com/public/banner.png";
 
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
+
 const Banner = (props) => {
-    const {src = defaultImage, title = defaultTitle, backgroundImageUrl = defaultBackgroundImageUrl} = props;
+    const { src = defaultImage, title = defaultTitle, backgroundImageUrl = defaultBackgroundImageUrl } = props;
+
     return (
-        <Paper style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "flex-end",
-            height: "90vh",
-            width: "100%",
-            backgroundImage: `url(${backgroundImageUrl})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            borderRadius: "0",
-        }}>
-            <div className="logo-text">
+        <ThemeProvider theme={darkTheme}>
+            <Paper sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "flex-end",
+                height: "90vh",
+                width: "100%",
+                backgroundImage: `url(${backgroundImageUrl})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                borderRadius: "0",
+            }}>
+                <div style={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "80%",
+                    justifyContent: "center",
+                    display: "flex",
+                    alignItems: "center"
+                }}>
 
-                <CardMedia component="img" src={src} alt={title} sx={{
-                    alignSelf: "center",
-                    width: {
-                        xs: '12rem',
-                        sm: '20rem',
-                        md: '22rem',
-                        lg: '25rem',
-                    }
-                }} >
-                </CardMedia>
-                <Typography
-                    variant="h1"
-                    sx={{
+                    <CardMedia component="img" src={src} alt={title} sx={{
+                        alignSelf: "center",
+                        width: {
+                            xs: '12rem',
+                            sm: '20rem',
+                            md: '22rem',
+                            lg: '25rem',
+                        }
+                    }} >
+                    </CardMedia>
+                    <Typography
+                        variant="h1"
+                        sx={{
+                            position: "absolute",
+                            paddingLeft: "2rem",
+                            paddingRight: "2rem",
+                            backgroundColor: "rgba(224,3,146, 0.6)",
+                            color: 'white',
+                            textAlign: 'center',
+                            whiteSpace: 'nowrap',
+                            fontWeight: "bold",
+                            fontSize: {
+                                xs: '3.5rem',
+                                sm: '4rem',
+                                md: '5rem',
+                                lg: '6rem',
+                                xl: '6rem',
+                            },
+                        }}
+                    >
+                        {title}
+                    </Typography>
+
+                </div>
+
+                <img
+                    alt="triangle"
+                    src={triangleIcon}
+
+                    style={{
+                        width: "63px",
+                        height: "66px",
                         position: "absolute",
-                        paddingLeft: "2rem",
-                        paddingRight: "2rem",
-                        fontFamily: 'Arial',
-                        backgroundColor: "rgba(224,3,146, 0.6)",
-                        color: 'white',
-                        textAlign: 'center',
-                        whiteSpace: 'nowrap',
-                        fontSize: {
-                            xs: '2.5rem',
-                            sm: '5rem',
-                            md: '5rem',
-                            lg: '6rem',
-                            xl: '6rem',
-                        },
+                        alignSelf: "flex-end",
+                        marginBottom: "3rem",
+                        transition: "margin-bottom 0.3s ease",
                     }}
-                >
-                    {title}
-                </Typography>
+                    onMouseEnter={(e) => {
+                        e.currentTarget.style.marginBottom = "2rem";
+                    }}
+                    onMouseLeave={(e) => {
+                        e.currentTarget.style.marginBottom = "3rem";
+                    }}
+                />
 
-            </div>
 
-            <img alt="triangle" src={triangleIcon} width="73" height="70" className="triangle" />
-
-        </Paper >
+            </Paper >
+        </ThemeProvider>
 
     )
-}
+};
 
 export default Banner
