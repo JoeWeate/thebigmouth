d1667889889a12ac85de7f31f97c2f4a86b0db4d
Olha Danylevska <odanilevskaya2@gmail.com>
2023-11-10 17:01:01 +0000

    Show more button
    added animation when hover

--- src/components/ShowMore.js
+++ src/components/ShowMore.js
@@ -1,58 +1,30 @@
 import React, { useState } from "react";
 import { Grid } from "@mui/material";
-import styles from "./ShowMore.module.css";
+import styles from "./SowMore.module.css";
 
 function ShowMore() {
   const [showContent, setShowContent] = useState(false);
 
-  const crossStyle = {
-    width: "45px",
-    height: "3px",
-    background: "white",
-    position: "relative",
-  };
-
-  const lineStyle = {
-    position: "absolute",
-    width: "70%",
-    height: "3px",
-    background: "white",
-  };
-  const verticalBarStyle = {
-    ...crossStyle,
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-  };
-
-  const horizontalBarStyle = {
-    ...crossStyle,
-    transform: "rotate(90deg)",
-  };
-
-
-  const rightLine = {
-    ...lineStyle,
-    bottom: -11,
-    right: -4,
-    transformOrigin: "bottom rigth",
-    transform: "rotate(135deg)",
-  };
-
   return (
-    <Grid container justifyContent="center" sx={{ height: "6rem" }}>
-      <div style={crossStyle}>
-        <div style={verticalBarStyle}></div>
-        <div style={horizontalBarStyle}></div>
-        <div
-          className={`${styles["left-line"]} ${showContent ? styles["left-line-animation"] : ""}`}
-          onMouseEnter={() => setShowContent(true)}
-          onMouseLeave={() => setShowContent(false)}
-        ></div>
-        <div style={rightLine}></div>
-      </div>
-    </Grid>
+    <Grid container item justifyContent="center" direction="column" alignItems="center" sx={{ height: "10rem", }}
+    >
+      <Grid container justifyContent="center" direction="column" gap={4} alignItems="center" sx={{ height: "8rem", width: "8rem" }}
+        onMouseEnter={() => setShowContent(true)}
+        onMouseLeave={() => setShowContent(false)}
+      >
+        <div className={`${styles["cross-style"]}`}>
+          <div className={`${showContent ? styles["vertical-bar-style-hover"] : styles["vertical-bar-style"]}`}></div>
+          <div className={`${showContent ? styles["horizontal-bar-style-hover"] : styles["horizontal-bar-style"]}`}></div>
+          <div
+            className={`${styles["left-line"]} ${showContent ? styles["left-line-animation"] : ""}`}
+          ></div>
+          <div className={`${styles["right-line"]} ${showContent ? styles["right-line-animation"] : ""}`}></div>
+
+        </div>
+        <div style={showContent ? { color: "rgb(10, 210, 183)" } : { color: "white" }}>SHOW MORE</div>
+
+      </Grid>
+    </Grid >
 
   );
 
