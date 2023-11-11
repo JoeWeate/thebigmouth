9272076512e6af635a93d9c58e93009c562cde50
Iryna Lypnyk <iryna.lypnyk@gmail.com>
2023-11-11 12:02:26 +0200

    update multimedia page - css, http-requests and api

--- src/pages/MultimediaPage/index.js
+++ src/pages/MultimediaPage/index.js
@@ -1,132 +1,97 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import {Typography} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import Grid from "@mui/material/Grid";
-import { isEmpty } from "lodash";
+import {isEmpty} from "lodash";
+import {getMultimedia, getOneMultimedia} from "../../api/multimedia";
 import BannerMultimedia from "./BannerMultimedia";
 import AboutInfo from "../../components/AboutInfo";
 import Information from "./Information";
-import configureAxios from "../../api/configureAxios";
-
-import ScrollMultimedia from "./ScrollMultimedia"; // Import ScrollMultimedia component
-import axios from "axios";
-import { Link } from "react-router-dom";
+import ScrollMultimedia from "./ScrollMultimedia";
+import {Link} from "react-router-dom";
 import VideoSection from "../HomePage/VideoSection";
-import styles from "./index.module.css";
 
 const MultimediaPage = () => {
-  const params = useParams();
-  const { ID } = params;
-  const [media, setMedia] = useState({});
-  const [episodes, setEpisodes] = useState([]);
-  const [isLoadingMedia, setIsLoadingMedia] = useState(true);
+    const params = useParams();
+    const {ID} = params;
+    const [media, setMedia] = useState({});
+    const [mediaList, setMediaList] = useState({});
+    const [episodes, setEpisodes] = useState([]);
+    const [isLoadingMedia, setIsLoadingMedia] = useState(true);
 
-  useEffect(() => {
-    const axios = configureAxios();
-    axios
-      .get(`/multimedia`)
-      .then((response) => {
-        const { multimedia } = response.data || {};
-        const selectedMedia = multimedia.find((item) => item.ID === ID);
-        setMedia(selectedMedia);
-        setEpisodes(selectedMedia.episodes);
-        setIsLoadingMedia(false);
-      })
-      .catch((error) => {
-        console.log("Error fetching multimedia data:", error);
-        setIsLoadingMedia(false);
-      });
-  }, [ID]);
-
-
-  useEffect(() => {
-    const fetchDetailedData = async () => {
-      try {
-        const response = await axios.get(
-          `https://bthgkjfail.execute-api.eu-west-2.amazonaws.com/v1/multimedia/${ID}`
-        );
-        const { episodes: detailedEpisodes, multimedia: detailedMedia } =
-          response.data || {};
-        setEpisodes(detailedEpisodes);
-        const selectedMedia = detailedMedia.find((item) => item.ID === ID);
-        setMedia(selectedMedia);
-        setIsLoadingMedia(false);
-      } catch (error) {
-        console.error("Error fetching detailed multimedia data:", error);
-        setIsLoadingMedia(false);
-      }
-    };
-
-    if (ID) {
-      fetchDetailedData();
-    }
-  }, [ID]);
-
-  const [multimediaData, setMultimediaData] = useState([]);
-
-  useEffect(() => {
-    const axios = configureAxios();
-    axios
-      .get(`/multimedia`)
-      .then((response) => {
-        if (response.data) {
-          setMultimediaData(response.data.multimedia);
-          console.log("Fetched Multimedia Data:", response.data.multimedia);
+    useEffect(() => {
+        setIsLoadingMedia(true);
+        try {
+            getOneMultimedia(ID).then(data => {
+                setMedia(data.multimedia);
+                setEpisodes(data.episodes);
+            });
+        } catch (error) {
+            console.log({error});
+        } finally {
+            setIsLoadingMedia(false);
         }
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+    }, [])
+
+    useEffect(() => {
+        setIsLoadingMedia(true);
+        try {
+            getMultimedia().then(data => {
+                setMediaList(data.multimedia)
+            });
+        } catch (error) {
+            console.log({error});
+        } finally {
+            setIsLoadingMedia(false);
+        }
+    }, [])
 
 
-  return (
-    <>
-      <Grid
-        container
-        sx={{ backgroundColor: "#2B2B2B" }}
-        className={styles.container}
-      >
-        <Grid item xs={12}>
-          {!isLoadingMedia && isEmpty(media) && (
-            <p>Try another ID! There is no media with ID {ID}</p>
-          )}
-          {!isLoadingMedia && !isEmpty(media) && (
-            <BannerMultimedia src={media.Images} alt={media.Name} />
-          )}
+    return (
+        <Grid
+            container
+            sx={{backgroundColor: "#2B2B2B",}}
+        >
+            {isLoadingMedia && isEmpty(media) && (
+                <Grid item xs={12} sx={{padding: "2rem"}}>
+                    <Typography sx={{color: "white"}} variant="h5" component='p'>Data is loading...</Typography>
+                </Grid>
+            )}
+            {!isLoadingMedia && isEmpty(media) && (
+                <Grid item xs={12} sx={{padding: "2rem"}}>
+                    <Typography sx={{color: "white"}} variant="h5" component='p'>Try another ID! There is no media with ID {ID}</Typography>
+                </Grid>
+            )}
+            {!isLoadingMedia && !isEmpty(media) && (
+                <>
+                    <Grid item xs={12}>
+                        <BannerMultimedia src={media.Images} alt={media.Name}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <AboutInfo episode={media.Description}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Link to={`/episode/${media.ID}?episode_id=S01E01`}>
+                            <ScrollMultimedia episodes={episodes} seriesId={media.ID}/>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Information
+                            released={media.Released}
+                            rated={media.Rated}
+                            regionOfOrigin={media.RegionOfOrigin}
+                            originalAudio={media.OriginalAudio}
+                        />
+                    </Grid>
+                    {!isEmpty(mediaList) && (
+                        <Grid item xs={12}>
+                            <VideoSection sectionTitle="Related" multimediaData={mediaList}/>
+                        </Grid>
+                    )}
+                </>
+            )}
         </Grid>
-        <Grid item xs={12}>
-        <AboutInfo episode={media.Description} />
-        {/* <p >{media.Description?.S}</p> */}
-      </Grid>
-
-
-        <Grid item xs={12}>
-
-          {!isLoadingMedia && !isEmpty(media) && (
-            <Link to={`/episode/${media.ID}?episode_id=S01E01`}>
-            <ScrollMultimedia episodes={episodes} seriesId={media.ID} />
-            </Link>
-          )}
-        </Grid>
-
-
-    <Grid item xs={12}>
-    {!isLoadingMedia && !isEmpty(media) && (
-      <Information
-        released={media.Released}
-        rated={media.Rated}
-        regionOfOrigin={media.RegionOfOrigin}
-        originalAudio={media.OriginalAudio}
-      />
-    )}
-  </Grid>
-     <VideoSection
-        sectionTitle="Related"
-        multimediaData={multimediaData}
-      />
-      </Grid>
-    </>
-  );
+    );
 };
 
 export default MultimediaPage;
