8310ae728a57afa0c560bd6a92e047bb1b63a1c5
Olha Danylevska <odanilevskaya2@gmail.com>
2023-11-12 23:41:13 +0000

    last changes
    xs screen not 100vh anymore
    Replaced Greed container to Grid Item
    Removed additional Grid item (as a gap)

--- src/pages/HomePage/SideBySide.js
+++ src/pages/HomePage/SideBySide.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Box, Grid, Hidden } from "@mui/material";
+import { Container, Box, Grid } from "@mui/material";
 
 const SideBySide = ({ targetRef }) => {
   const [wordIndex, setWordIndex] = useState(0);
@@ -23,15 +23,15 @@ const SideBySide = ({ targetRef }) => {
     <Container
       ref={targetRef}
       sx={{
-        minHeight: "100vh",
-        height: "100%",
+        minHeight: { lg: "100vh" },
+        height: { xs: "auto", sm: "100%" },
         display: "flex",
         justifyContent: "center",
-        marginBottom: "2.3rem",
+        marginBottom: { lg: "2rem" },
       }}
     >
-      <Grid container alignItems="center" justifyContent="center">
-        <Grid item xs={11} lg={6} alignItems="center" justifyContent="center" sx={{ marginTop: { lg: 0, md: "2rem", sm: "2rem", xs: "1rem" }, marginBottom: { lg: "1rem", md: "1rem", sm: "2rem" } }}>
+      <Grid container alignItems="center" justifyContent="center" gap={1}>
+        <Grid item xs={11} lg={6} sx={{ marginTop: { lg: 0, md: "2rem", sm: "2rem", xs: "3rem" }, marginBottom: { lg: "1rem", md: "1rem", sm: "1rem", xs: 0 } }}>
           <Box
             sx={{
               display: "flex",
@@ -64,55 +64,56 @@ const SideBySide = ({ targetRef }) => {
 
           </Box>
         </Grid>
-        <Grid item xs={0} lg={1} sx={{ marginRight: { lg: "-3rem" } }}></Grid>
-        <Grid container xs={11} lg={5} sx={{ justifyContent: { xs: "left", sm: "center" }, paddingRight: { xs: 0, lg: "2rem" } }}>
-          <Box
-            sx={{
-              display: "flex",
-              justifySelf: "center",
-              justifyContent: "center",
-              borderRadius: 0,
-              transition: "0.4s",
-              transitionTimingFunction: "ease-out",
-              height: { xs: "250px", sm: "450px" },
-              maxHeight: "450px",
-              width: { xs: "450px", sm: "450px" },
-              maxWidth: "450px",
-              textAlign: "center",
-              border: { xs: 0, sm: 2 },
-              color: "#E6007E",
-              padding: { xs: 0, sm: "30px" },
-              ":hover": {
-                borderRadius: "50%",
-                transition: "0.4s",
-                transitionTimingFunction: "ease-in",
-              },
-            }}
-          >
+
+        <Grid item xs={11} lg={5} sx={{ pr: { xs: 0, lg: "2rem" }, mb: { sm: "3rem", xs: "4rem" }, mt: { sm: "2rem", xs: "2rem" }, }} >
+          <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
             <Box
-              color="#fff"
               sx={{
-                textAlign: "left",
+
                 display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                fontSize: "17px",
+                borderRadius: 0,
+                transition: "0.4s",
+                transitionTimingFunction: "ease-out",
+                height: { xs: "100%", sm: "450px" },
+                maxHeight: "450px",
+                width: { xs: "100%", sm: "450px" },
+                maxWidth: "450px",
+                textAlign: "center",
+                border: { xs: 0, sm: 2 },
+                color: "#E6007E",
                 padding: { xs: 0, sm: "30px" },
+                ":hover": {
+                  borderRadius: "50%",
+                  transition: "0.4s",
+                  transitionTimingFunction: "ease-in",
+                },
               }}
             >
-              <p>
-                Welcome to The Big Mouth, the place where The Big House's
-                digital content lives. It's still early days, and this is just
-                the start of some big things to come. Read more about what
-                we have
-                <span style={TextColor}> got planned </span>
-                or take a look below at what we have for you right now.
-              </p>
+              <Box
+                color="#fff"
+                sx={{
+                  textAlign: "left",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  fontSize: "17px",
+                  padding: { xs: 0, sm: "30px" },
+                }}
+              >
+                <p>
+                  Welcome to The Big Mouth, the place where The Big House's
+                  digital content lives. It's still early days, and this is just
+                  the start of some big things to come. Read more about what
+                  we have
+                  <span style={TextColor}> got planned </span>
+                  or take a look below at what we have for you right now.
+                </p>
+              </Box>
             </Box>
           </Box>
         </Grid>
       </Grid>
-    </Container>
+    </Container >
   );
 }
 
