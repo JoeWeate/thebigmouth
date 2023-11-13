b6dc25408b254ba2c06b74b5981a58318ca9127b
Olha Danylevska <odanilevskaya2@gmail.com>
2023-11-13 13:34:17 +0000

    on  multimedia page reorganise Information component
    replaced dropdown arrow, added colour on hover

--- src/pages/MultimediaPage/Information.js
+++ src/pages/MultimediaPage/Information.js
@@ -1,155 +1,96 @@
-import {Button, Divider, Typography, Collapse} from '@mui/material';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import './Information.css';
-import {useState} from 'react';
+import React, { useState } from 'react';
+import { Button, Divider, Typography, Collapse, Grid, Container, Box } from '@mui/material';
 
-const Information = ({released, rated, regionOfOrigin, originalAudio}) => {
+
+const Information = ({ released, rated, regionOfOrigin, originalAudio }) => {
     const [open, setOpen] = useState(false);
-    const [disable, setDisable] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
+
 
     const handleClick = () => {
-        setOpen(true);
+        setOpen(!open);
     };
 
-    const handleClickUp = () => {
-        setOpen(false);
-    };
+    const disableButton = !rated;
 
     return (
-        <div className="info-wrapper">
-            <Divider sx={
-                {backgroundColor: 'white'}
-            }></Divider>
-            <div className="info-container">
-                <Typography className="info"
-                    sx={
-                        {color: 'White'}
-                }>
-                    <p>INFORMATION</p>
-                </Typography>
-                {
-                !rated && setDisable(true)
-            }
-                {
-                open ? (
-                    <Button className="button-2" variant="text" disableRipple
-                        onClick={handleClickUp}
-                        sx={
-                            {ml: 1}
-                    }>
-                        <KeyboardArrowUpIcon sx={
-                            {
-                                fontSize: 40,
-                                color: 'white'
-                            }
-                        }/>
-                    </Button>
-                ) : (
-                    <Button disable={
-                            disable ? true : undefined
-                        }
-                        className="button-2"
-                        variant="text"
-                        disableRipple
-                        onClick={handleClick}
-                        sx={
-                            {ml: 1}
-                    }>
-                        <KeyboardArrowDownIcon sx={
-                            {
-                                fontSize: 40,
-                                color: 'white'
-                            }
-                        }/>
-                    </Button>
-                )
-            } </div>
+        <Container sx={{ width: { lg: "100%", xs: "90%" } }}>
+            <Divider sx={{ backgroundColor: 'white' }} />
+            <Button
+                disabled={disableButton}
+                variant="text"
+                disableRipple
+                onClick={handleClick}
+                sx={{
+                    "&:hover": {
+                        backgroundColor: "transparent"
+                    }
+                }}
+
+            >
+                <Grid container direction="row" mt={4} mb={3.5} justifyContent="center" flexWrap="nowrap" gap="1rem">
+                    <Grid item>
+                        <Typography className="info" sx={{ color: 'white' }}>
+                            <p>INFORMATION</p>
+                        </Typography>
+                    </Grid>
+                    <Grid item sx={{ pt: "0.2rem" }}>
+                        <svg
+                            fill={isHovered ? '#E8FC02' : 'white'}
+                            width="1.2rem"
+                            version="0.8"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                            viewBox="0 0 330 330"
+                            xmlSpace="preserve"
+                            style={{
+                                transform: `rotate(${open ? '180deg' : '0deg'})`,
+                                transition: 'fill 0.3s ease',
+                            }}
+                            onMouseEnter={() => setIsHovered(true)}
+                            onMouseLeave={() => setIsHovered(false)}
+                        >
+                            <path
+                                id="XMLID_225_"
+                                d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393 c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393 s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+                            />
+                        </svg>
+                    </Grid>
+                </Grid>
+            </Button>
+
+
             <Collapse in={open}>
-                <div className="item">
-                    <Typography sx={
-                        {
-                            fontSize: 18,
-                            fontWeight: 600,
-                            color: 'white'
-                        }
-                    }>
+                <Divider sx={{ marginBottom: '1rem', backgroundColor: 'white' }} />
+
+                <Box sx={{ mt: "3rem" }}>
+                    <Typography sx={{ fontSize: 14, color: 'white' }}>
+                        <h4>Released</h4>
+                        <p style={{ fontWeight: "initial" }}>{released}</p>
                     </Typography>
-                    <Typography sx={
-                        {
-                            fontSize: 14,
-                            marginTop: '-1.5rem',
-                            color: 'white'
-                        }
-                    }>
-                        <p> Released {released}</p>
+                </Box>
+                <Box sx={{ mt: "1.5rem" }}>
+                    <Typography sx={{ fontSize: 14, color: 'white' }}>
+                        <h4>Rated</h4>
+                        <p style={{ fontWeight: "lighter" }}>{rated}</p>
                     </Typography>
-                </div>
-                <div className="item">
-                    <Typography sx={
-                        {
-                            fontSize: 18,
-                            fontWeight: 600,
-                            color: 'white'
-                        }
-                    }>
+                </Box>
+                <Box sx={{ mt: "1.5rem" }}>
+                    <Typography sx={{ fontSize: 14, color: 'white' }}>
+                        <h4>Region of Origin</h4>
+                        <p style={{ fontWeight: "lighter" }}>{regionOfOrigin}</p>
                     </Typography>
-                    <Typography sx={
-                        {
-                            fontSize: 14,
-                            marginTop: '-1.5rem',
-                            color: 'white'
-                        }
-                    }>
-                        <p>Rated {rated}</p>
+                </Box>
+                <Box sx={{ mt: "1.5rem", mb: "2rem" }}>
+                    <Typography sx={{ fontSize: 14, color: 'white' }}>
+                        <h4>Original Audio</h4>
+                        <p style={{ fontWeight: "lighter" }}>{originalAudio}</p>
                     </Typography>
-                </div>
-                <div className="item">
-                    <Typography sx={
-                        {
-                            fontSize: 18,
-                            fontWeight: 600,
-                            color: 'white'
-                        }
-                    }>
-                    </Typography>
-                    <Typography sx={
-                        {
-                            fontSize: 14,
-                            marginTop: '-1.5rem',
-                            color: 'white'
-                        }
-                    }>
-                        <p>Region of Origin {regionOfOrigin}</p>
-                    </Typography>
-                </div>
-                <div className="item">
-                    <Typography sx={
-                        {
-                            fontSize: 18,
-                            fontWeight: 600,
-                            color: 'white'
-                        }
-                    }>
-                    </Typography>
-                    <Typography sx={
-                        {
-                            fontSize: 14,
-                            marginTop: '-1.5rem',
-                            color: 'white'
-                        }
-                    }>
-                        <p>Original Audio {originalAudio}</p>
-                    </Typography>
-                </div>
+                </Box>
             </Collapse>
-            <Divider sx={
-                {
-                    marginBottom: '1rem',
-                    backgroundColor: 'white'
-                }
-            }></Divider>
-        </div>
+
+            <Divider sx={{ marginBottom: '1rem', backgroundColor: 'white' }} />
+        </Container >
     );
 };
 
