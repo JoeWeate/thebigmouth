b46b74ca749974c63c8cf902bfef7752a2a810a0
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-11-15 15:24:16 +0000

    responsive footer improvement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,232 +1,14 @@
 import React from "react";
-import { Container, Grid, Box, CssBaseline, Typography } from "@mui/material";
+import { Grid, Box, CardMedia, Typography, Paper, Hidden } from "@mui/material";
 import Link from "@mui/material/Link";
 import theme from "../theme";
-import { styled } from "@mui/system";
+import { styled, width } from "@mui/system";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import tiktok from "../assets/images/tiktok.svg";
-const FullFooterContainer = styled("div")`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100vh;
-  margin-top: 2em;
-`;
 
-const FooterContainer = styled("footer")`
-  display: flex;
-  background-color: #e6007e;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 280px;
-  padding: 0 20px;
-  -bottom: 1em;
-
-  @media (min-width: ${theme.breakpoints.values.xs}px) {
-    height: 500px;
-    width: 100vh;
-  }
-`;
-
-const StyledBox = styled(Box)`
-  display: flex;
-  // height: 280px;
-  flex-direction: center;
-  align-items: center;
-  justify-content: space-between;
-  height: 100%;
-
-  // padding-right: 50px;
-
-  @media (max-width: ${theme.breakpoints.values.xs}px) {
-    flex-direction: column;
-    align-items: center;
-  }
-  @media (max-width: ${theme.breakpoints.values.md}px) {
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-const LogoContainer = styled(Box)`
-  position: relative;
-  @media (max-width: ${theme.breakpoints.values.xs}px) {
-    width: 314.24px;
-    height: 222.19px;
-  }
-`;
-
-const LogoTextContainer = styled(Box)`
-  // position: relative;
-  // margin-right: 4em;
-  // width: 314px;
-  // height: 64.65px;
-  // left: 37.88px;
- 
-  @media (max-width: ${theme.breakpoints.values.xs}px) {
-    margin-right: 0;
-  // }
-   @media (max-width: ${theme.breakpoints.values.md}px) {
-  //   // margin-bottom: 0.5em;
-  //   margin-right: 0;
-  // }
-  position: relative;
-  display: flex;
-  backdrop-filter: blur(5px);
-  opacity: 77%;
-  filter: brightness(33%) 
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  // margin-top: -30px;
-  @media (max-width: ${theme.breakpoints.values.xs}px) {
-    margin-top: 0;
-  }
-`;
-
-const LogoImage = styled("img")`
-  width: 237px;
-  height: 273px;
-  background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
-  opacity: 1;
-
-  @media (min-width: ${theme.breakpoints.values.xs}px) {
-    height: 230px;
-    width: 100%;
-  }
-
-  @media (min-width: ${theme.breakpoints.values.md}px) {
-    // height: 200px;
-    width: auto;
-  }
-
-  @media (min-width: ${theme.breakpoints.values.lg}px) {
-    height: 279.72px;
-    width: 100%;
-  }
-`;
-
-const LogoText = styled(Typography)`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background: #e6007e;
-  font-weight: bold;
-  color: #ffffff;
-  white-space: nowrap;
-  letter-spacing: -2.04px;
-  z-index: 1;
-  background: rgba(230, 0, 126, 0.73);
-
-  @media (min-width: ${theme.breakpoints.values.xs}px) {
-    font-size: 41px;
-  }
-  @media (min-width: ${theme.breakpoints.values.sm}px) {
-    font-size: 35px;
-  }
-
-  // @media (min-width: ${theme.breakpoints.values.md}px) {
-  //   top: 50%;
-  //   left: 50%;
-  // }
-
-  @media (min-width: ${theme.breakpoints.values.lg}px) {
-    font-size: 51px;
-  }
-`;
-
-const FooterLinkContainer = styled(Box)`
-  display: flex;
-  align-items: center;
-
-  @media (min-width: ${theme.breakpoints.values.xs}px) {
-    flex-direction: column;
-    column-gap: 10px;
-    width: 319px;
-    height: 183px;
-
-    @media (min-width: ${theme.breakpoints.values.sm}px) {
-      flex-direction: row;
-      justify-content: space-between;
-      padding-bottom: 1em;
-      justify-content: center;
-      // margin-right: 4em;
-    }
-
-    // @media (min-width: ${theme.breakpoints.values.md}px) {
-    // margin-left: auto;
-    //   flex-direction: row;
-    //   justify-content: space-around;
-    //   align-items: center;
-    // }
-    @media (max-width: ${theme.breakpoints.values.md}px) {
-      flex-direction: column;
-      align-items: center;
-    }
-  }
-
-  @media (min-width: ${theme.breakpoints.values.lg}px) {
-    margin-left: auto;
-    text-align: left;
-  }
-`;
-
-const FooterLink = styled(Link)`
-  color: #e0e0e0;
-  text-decoration: none;
-  display: flex;
-  background-color: transparent;
-  // margin: 0 2em;
-
-  @media (min-width: ${theme.breakpoints.values.sm}px) {
-    padding-top: 2em;
-  }
-  @media (min-width: ${theme.breakpoints.values.xs}px) {
-    whites-space: no-wrap;
-    flex-shrink: 0;
-    // margin-bottom: 35px;
-    font-size: 22px;
-    line-height: 2em;
-    text-wrap: nowrap;
-    line-height:;
-  }
-`;
-const SocialMediaContainer = styled(Box)`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin-bottom: 1em;
-  font-size: 35px;
-
-  @media (min-width: ${theme.breakpoints.values.xs}px) {
-    margin-top: 0.5em;
-    width: 319px;
-    height: 35.18px;
-  }
-`;
-
-const SocialMediaLink = styled(Link)`
-  && {
-    color: #e0e0e0;
-    text-decoration: none;
-    display: flex;
-    background-color: transparent;
-    padding: 8px;
-    height: 35px;
-  }
-`;
-const CustomIcon = styled("div")`
-  .MuiSvgIcon-root {
-    font-size: 35px !important;
-  }
-`;
 const CopyrightContainer = styled(Box)`
   background-color: #2b2b2b;
   color: #ffffff;
@@ -247,66 +29,202 @@ const CopyrightContainer = styled(Box)`
   }
 `;
 const Footer = () => {
-  return (
-    <FullFooterContainer>
-      <FooterContainer>
-        <CssBaseline />
-        <Container maxWidth="lg">
-          <Grid>
-            <StyledBox item xs={12} md={4}>
-              <LogoContainer>
-                <LogoImage
-                  src="https://thebigmouth-media.s3.eu-west-2.amazonaws.com/public/big-mouth.png"
-                  alt="Logo"
-                />
-                <LogoTextContainer>
-                  <LogoText variant="body1">THE BIG MOUTH</LogoText>
-                </LogoTextContainer>
-              </LogoContainer>
-              <FooterLinkContainer>
-                <FooterLink href="/back-to-the-big-mouth">
-                  Back to The Big Mouth
-                </FooterLink>
-                <FooterLink href="/become-a-member">Become a member</FooterLink>
-                <FooterLink href="/contact">Contact</FooterLink>
-                <FooterLink href="/talent-enquiry">Talent Enquiry</FooterLink>
-              </FooterLinkContainer>
-              <SocialMediaContainer>
-                <CustomIcon>
-                  <SocialMediaLink href="#tiktok">
-                    <img src={tiktok} alt="TikTok" width="30.33" height="35" />
-                  </SocialMediaLink>
-                </CustomIcon>
-                <CustomIcon>
-                  <SocialMediaLink href="#facebook">
-                    <FacebookIcon />
-                  </SocialMediaLink>
-                </CustomIcon>
-                <CustomIcon>
-                  <SocialMediaLink href="#twitter">
-                    <TwitterIcon />
-                  </SocialMediaLink>
-                </CustomIcon>
+  const src =
+    "https://thebigmouth-media.s3.eu-west-2.amazonaws.com/public/big-mouth.png";
+  const title = "THE BIG MOUTH";
 
-                <CustomIcon>
-                  <SocialMediaLink href="#instagram">
-                    <InstagramIcon />
-                  </SocialMediaLink>
-                </CustomIcon>
-                <CustomIcon>
-                  <SocialMediaLink href="#linkedin">
-                    <LinkedInIcon />
-                  </SocialMediaLink>
-                </CustomIcon>
-              </SocialMediaContainer>
-            </StyledBox>
+  return (
+    <Paper
+      sx={{
+        marginTop: "calc(10% + 60px)",
+        width: "100%",
+        position: "relative",
+        bottom: 0,
+      }}
+      component="footer"
+      square
+      // variant="outlined"
+    >
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="space-around"
+        lg={12}
+        sx={{
+          backgroundColor: "#E6007E",
+          flexDirection: "row",
+          height: { lg: "280px", md: "400px", sm: "600px" },
+        }}
+      >
+        <Grid
+          item
+          lg={4}
+          xs={12}
+          alignItems="center"
+          justifyConten="center"
+          // sx={{ height: "279px" }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              height: "100%",
+              justifyConten: "center",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <CardMedia
+              component="img"
+              src={src}
+              alt={title}
+              sx={{
+                alignSelf: "center",
+                maxWidth: {
+                  xs: "40%",
+                  sm: "30%",
+                  md: "25%",
+                  lg: "40%",
+                },
+                paddingBottom: {
+                  md: "3em",
+                  lg: "3em",
+                },
+              }}
+            ></CardMedia>
+            <Typography
+              variant="h8"
+              sx={{
+                position: "absolute",
+                paddingLeft: "1rem",
+                paddingRight: "1rem",
+                background: "rgba(230, 0, 126, 0.73)",
+                fontWeight: "bold",
+                backdropFilter: "blur(5px)",
+                color: "white",
+                letterSpacing: " -2.04px",
+                textAlign: "center",
+                whiteSpace: "nowrap",
+                fontSize: {
+                  xs: "2.5rem",
+                  sm: "3rem",
+                  md: "2.5rem",
+                  lg: "2.5rem",
+                },
+                mb: {
+                  xs: "0.3em",
+                  md: "1em",
+                },
+              }}
+            >
+              {title}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={4} sm={8} md={8} lg={8}>
+          <Grid
+            container
+            justifyContent={{ xs: "center", sm: "center", md: "center" }}
+            alignItems="center"
+            sx={{
+              width: "100%",
+              gap: {
+                xs: "2.6rem",
+                sm: "0.5rem",
+              },
+              mb: "1rem",
+            }}
+          >
+            <Link
+              sx={{
+                textAlign: "center",
+                color: "white",
+                textDecoration: "none",
+              }}
+              href="/back-to-the-big-mouth"
+            >
+              Back to The Big Mouth
+            </Link>
+            <Link
+              href="/become-a-member"
+              sx={{
+                textAlign: "center",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              Become a member
+            </Link>
+            <Link
+              href="/contact"
+              sx={{
+                textAlign: "center",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              Contact
+            </Link>
+            <Link
+              href="/talent-enquiry"
+              sx={{
+                textAlign: "center",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              Talent Enquiry
+            </Link>
           </Grid>
-        </Container>
-      </FooterContainer>
-      <CopyrightContainer>
-        © The Big House Theatre | Charity Number 1151106.
-      </CopyrightContainer>
-    </FullFooterContainer>
+        </Grid>
+        <Hidden smUp>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            sm={12}
+            sx={{
+              mt: "1rem",
+              mb: "4rem",
+              ml: "6rem",
+              mr: "4rem",
+              zIndex: 1,
+            }}
+          >
+            <Link href="#tiktok">
+              <img src={tiktok} alt="TikTok" width="30" height="35" />
+            </Link>
+            <Link href="#facebook">
+              <FacebookIcon style={{ fontSize: "2rem", color: "white" }} />
+            </Link>
+            <Link href="twitter">
+              <TwitterIcon style={{ fontSize: "2rem", color: "white" }} />
+            </Link>
+            <Link href="#instagram">
+              <InstagramIcon style={{ fontSize: "2rem", color: "white" }} />
+            </Link>
+            <Link href="#linkedin">
+              <LinkedInIcon style={{ fontSize: "2rem", color: "white" }} />
+            </Link>
+          </Grid>
+        </Hidden>
+
+        <Grid
+          item
+          xs={12}
+          lg={0}
+          sx={{
+            bottom: 0,
+            width: "100%",
+            position: "absolute",
+          }}
+        >
+          <CopyrightContainer>
+            © The Big House Theatre | Charity Number 1151106.
+          </CopyrightContainer>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
