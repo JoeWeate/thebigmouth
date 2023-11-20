88610b3f9166aceed8ae95714135ba01b2aa5185
Iryna Lypnyk <iryna.lypnyk@gmail.com>
2023-11-20 14:56:52 +0200

    [mui theme]: fix white color Typography by default - switch on dark mode in MUI theme and add CssBaseline component from @mui/material

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,11 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
+    mode: "dark",
     background: {
       default: "#2B2B2B",
     },
-    primary: {
-      main: "rgba(0, 0, 0, 1)",
-    },
     text: {
-      main: "rgba(0, 0, 0, 1)",
-      primary: "#fff",
       secondary: "#8c8c8c",
     },
     yellow:{
@@ -92,6 +88,7 @@ const theme = createTheme({
     styleOverrides: {
       root: ({ theme }) => ({
         backgroundColor: theme.palette.background.default,
+        backgroundImage: "none",
         color: "white",
         boxShadow: "none",
         "&:not(:last-child)": {
