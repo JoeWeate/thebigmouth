abb8cda563f3874fea616b457dd17981574efa1b
Olha Danylevska <odanilevskaya2@gmail.com>
2023-11-20 18:09:42 +0000

    added button "add Your art"
    created simple animation

---
+++ src/components/AddArtButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./AddArtButton.module.css";
+import { Box } from "@mui/material";
+
+const AddArtButton = () => {
+    return (
+        <Box className={styles.box}>
+            <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Add Your Art
+            </a>
+        </Box>
+    );
+};
+
+export default AddArtButton;
