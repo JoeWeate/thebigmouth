6778ef346948683ea3739fd55c6fe0a5ee389aec
Olha Danylevska <odanilevskaya2@gmail.com>
2023-11-22 13:52:47 +0000

    added reusable PageTitleComponent
    in dummyHomePage/PageTitleComponent.js

--- src/pages/DummyHomePage/index.js
+++ src/pages/DummyHomePage/index.js
@@ -1,27 +1,24 @@
-import { Grid, Typography } from "@mui/material"
-import AddArtButton from "../../components/userHub/AddArtButton"
+import { Grid } from "@mui/material"
 import UserVideo from "../../components/userHub/UserVideo"
 import { userHubVideoListMocks } from "../../api/mocks"
+import PageTitleComponent from "./PageTitleComponent"
 
 const DummyHomePage = () => {
+    let title =
+    {
+        mainTitle: "Welcome to VideoHub",
+        subtitle: "Your Ultimate Destination for Awesome Content Experiences!"
+    }
+    let fontSize =
+    {
+        titleFontSize: "50pt",
+        subtitleFontSize: "24pt"
+    }
     return (
 
         <Grid container direction="column" justifyContent="center" alignItems="center">
-            <Grid item lg={12} mb="2rem" sx={{ mt: { xs: "1rem" }, mb: { xs: "1rem", lg: "2rem" } }} >
-                <Grid container direction="column" alignItems="center">
-                    <Typography variant="h5" sx={{ textAlign: "center", textTransform: "uppercase", fontWeight: "600", fontSize: { lg: "50pt", sm: "40pt", xs: "20pt" } }}  >
-                        <p >Welcome to <span style={{ color: "#E6007E" }}>Video</span>Hub</p>
-                    </Typography>
-
-                    <Typography variant="h4" sx={{ textAlign: "center", fontSize: { xs: "13pt", sm: "14pt", md: "24pt" }, marginTop: { lg: "-5rem", xs: "-2rem" }, marginBottom: { xs: "2rem", lg: "4rem" } }}>
-                        <p>
-                            Your Ultimate Destination for Awesome Content Experiences!
-                        </p>
-
-                    </Typography>
-
-                    <AddArtButton />
-                </Grid>
+            <Grid item lg={12} sx={{ mt: { xs: "2rem", lg: "2rem" }, mb: { xs: "1rem", lg: "2rem" } }} >
+                <PageTitleComponent title={title} fontSize={fontSize} />
             </Grid>
             <Grid item sx={{ width: "90%", marginTop: "2rem", marginBottom: "2rem" }}>
                 <Grid container justifyContent="center" sx={{ marginBottom: { lg: "4rem", sm: 0 } }}>
