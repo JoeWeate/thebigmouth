ca776222f8d71373539407fb571b175129f4acfe
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-11-27 22:39:59 +0000

    adding /videos endpoint mistakenly deleted it while merging

--- src/api/videos.js
+++ src/api/videos.js
@@ -1,5 +1,16 @@
 import configureAxios from "./configureAxios";
+
 const api = configureAxios({});
+
+export const uploadVideo = async (data) => {
+  try {
+    const response = await api.post("/videos", data);
+    return response.data;
+  } catch (error) {
+    console.error("Error uploading video:", error);
+    throw new Error("Failed to upload video. Please try again later.");
+  }
+};
 export const getVideos = () => {
   return api
     .get(`/videos`, {})
