8bff23fe17e9e8b759632b9a1755f9ea568b8164
AndriusIsin <isin.andrius@gmail.com>
2023-11-28 14:07:36 +0000

    Rebuilt the dashboard using Material-UI.

--- src/pages/Dashboard/AllUsersPage.js
+++ src/pages/Dashboard/AllUsersPage.js
@@ -1,14 +1,39 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const AllUsersPage = ({ state }) => {
   return (
-    <div
-      style={{
-        color: "black",
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        pt: 2,
+        width: "100%",
       }}
     >
-      Coming soon....
-    </div>
+      <Typography
+        variant="h3"
+        sx={{
+          color: "#ff5d8f",
+          textShadow: "1px 1px 2px rgba(0, 0, 0, 0.3)",
+          animation: "pulse 2s infinite ease-in-out",
+        }}
+      >
+        Coming soon...
+      </Typography>
+
+      <style>
+        {`
+          @keyframes pulse {
+            0% { transform: scale(1); }
+            50% { transform: scale(1.1); }
+            100% { transform: scale(1); }
+          }
+        `}
+      </style>
+    </Box>
   );
 };
 
