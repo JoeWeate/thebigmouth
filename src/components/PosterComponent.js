3f515ac1c1e4838a55e5183f4ef648d9802d0293
AndriusIsin <isin.andrius@gmail.com>
2023-12-02 12:23:48 +0000

    changed to react

--- src/components/PosterComponent.js
+++ src/components/PosterComponent.js
@@ -1,39 +1,48 @@
-import { Box, Grid, Avatar, Typography } from '@mui/material';
+import { Box, Grid, Avatar, Typography } from "@mui/material";
 
 const PosterComponent = ({ isFullScreen, XRayMocks, currentTime }) => {
-    return (
-        <Box
-            sx={{
-                display: "flex",
-                position: "absolute",
-                justifyContent: "center",
-                alignItems: "center",
-                margin: "0 auto",
-                bottom: 0,
-                left: 0,
-                maxWidth: isFullScreen ? "100%" : "640px",
-                height: "150px",
-                backgroundColor: "rgba(59, 59, 59, 0.7)"
-            }}
-        >
-            {XRayMocks.map((actor) => (
-                currentTime >= actor.start && currentTime <= actor.end && (
-                    <Grid container direction="row" lg={10} gap={4}>
-                        <Grid item lg={2} sx={{ marginBottom: "5rem" }}>
-                            <Avatar src={actor.actorImgSRC} sx={{ width: 85, height: 104, borderRadius: 1, border: "solid #E60077 1px" }}></Avatar>
-                        </Grid>
-                        <Grid item lg={9} sx={{ marginTop: "1.5rem" }}>
-                            <Typography variant="h6">{actor.name}</Typography>
-                            <Typography sx={{ fontSize: "10pt" }}>
-                                {actor.description}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                )
-            ))}
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        position: "absolute",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "0 auto",
+        bottom: 40,
+        left: 0,
+        maxWidth: isFullScreen ? "100%" : "640px",
+        height: "150px",
+        backgroundColor: "rgba(59, 59, 59, 0.7)",
+      }}
+    >
+      {XRayMocks.map(
+        (actor) =>
+          currentTime >= actor.start &&
+          currentTime <= actor.end && (
+            <Grid container direction="row" lg={10} gap={4}>
+              <Grid item lg={2} sx={{ marginBottom: "5rem" }}>
+                <Avatar
+                  src={actor.actorImgSRC}
+                  sx={{
+                    width: 85,
+                    height: 104,
+                    borderRadius: 1,
+                    border: "solid #E60077 1px",
+                  }}
+                ></Avatar>
+              </Grid>
+              <Grid item lg={9} sx={{ marginTop: "1.5rem" }}>
+                <Typography variant="h6">{actor.name}</Typography>
+                <Typography sx={{ fontSize: "10pt" }}>
+                  {actor.description}
+                </Typography>
+              </Grid>
+            </Grid>
+          )
+      )}
+    </Box>
+  );
 };
 
-export default PosterComponent
-
+export default PosterComponent;
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,97 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Grid } from '@mui/material';
-import XRayMocks from '../api/mocks';
-import PosterComponent from './PosterComponent';
+import React, { useState, useEffect } from "react";
+import { Typography, Grid } from "@mui/material";
+import ReactPlayer from "react-player";
+import XRayMocks from "../api/mocks";
+import PosterComponent from "./PosterComponent";
+import { Box } from "@mui/system";
 
-const VIDEO_SRC = 'https://thebigmouth-media.s3.eu-west-2.amazonaws.com/public/multimedia/longer_movie.mp4';
-
-console.log(XRayMocks, XRayMocks)
+const VIDEO_SRC =
+  "https://thebigmouth-media.s3.eu-west-2.amazonaws.com/public/multimedia/longer_movie.mp4";
 
 export default function VideoPlayer() {
   const [currentTime, setCurrentTime] = useState(0);
-  const [duration, setDuration] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isFullScreen, setIsFullScreen] = useState(false);
-
-  useEffect(() => {
-    const videoElement = document.getElementById('video-active');
-    if (!videoElement) {
-      console.error("Video element not found.");
-      return;
-    }
-
-
-    const onTimeUpdate = () => {
-      setCurrentTime(videoElement.currentTime);
-      setDuration(videoElement.duration);
-    };
-
-    const onPlay = () => {
-      setIsPlaying(true);
-    };
-
-    const onPause = () => {
-      setCurrentTime(videoElement.currentTime);
-      setIsPlaying(false);
-      toggleOverlay();
-    };
-    //check if IsFullScreen is true
-    const onFullScreenChange = () => {
-      if (document.fullscreenElement !== null) {
-        setIsFullScreen(true);
-      } else {
-        setIsFullScreen(false)
-      }
-
-    };
-
-    videoElement.addEventListener('timeupdate', onTimeUpdate);
-    videoElement.addEventListener('play', onPlay);
-    videoElement.addEventListener('pause', onPause);
-    document.addEventListener('fullscreenchange', onFullScreenChange);
-
-    return () => {
-      videoElement.removeEventListener('timeupdate', onTimeUpdate);
-      videoElement.removeEventListener('play', onPlay);
-      videoElement.removeEventListener('pause', onPause);
-      document.removeEventListener('fullscreenchange', onFullScreenChange);
-    };
-  }, [isFullScreen, currentTime]);
+  const [showOverlay, setShowOverlay] = useState(true);
 
   const toggleOverlay = () => {
-    const overlayId = isFullScreen ? 'overlay-fullscreen' : 'overlay';
-    const overlay = document.getElementById(overlayId);
-
-    console.log("Overlay display style:", overlay.style.display);
-    console.log("isPlaying:", isPlaying);
-    console.log("currentTime:", currentTime);
-
-    if (overlayId === "overlay-fullscreen") {
-      overlay.style.display = (!isFullScreen && isPlaying ? 'block' : 'none')
-    } else {
-      overlay.style.display = (isPlaying ? 'block' : 'none');
-    }
+    setShowOverlay(!showOverlay);
   };
 
+  const handleProgress = (progress) => {
+    setCurrentTime(progress.playedSeconds);
+  };
 
+  const handlePlay = () => {
+    setIsPlaying(true);
+    toggleOverlay();
+  };
+
+  const handlePause = () => {
+    setIsPlaying(false);
+    toggleOverlay();
+  };
+
+  const handleFullScreenChange = () => {
+    setIsFullScreen(!!document.fullscreenElement);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
+  useEffect(() => {
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullScreenChange);
+    };
+  }, []);
+  console.log(isFullScreen);
   return (
-    <Grid container sx={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
-      <Typography>Current Time: {currentTime}</Typography>
-      <Typography>Duration Time: {duration}</Typography>
-      <div id="overlay" className="overlay" onClick={toggleOverlay}>
-        {isPlaying ? null : <PosterComponent XRayMocks={XRayMocks} currentTime={currentTime} isFullScreen={isFullScreen} />}
-      </div>
-      <div id="overlay-fullscreen" className="overlay-fullscreen" onClick={toggleOverlay}>
-        {isFullScreen && !isPlaying && <PosterComponent XRayMocks={XRayMocks} currentTime={currentTime} isFullScreen={isFullScreen} />}
-      </div>
-      <video
-        id="video-active"
-        width="640"
-        height="390"
-        controls="controls"
-        src={VIDEO_SRC}
-        onClick={toggleOverlay}
+    <Grid
+      container
+      sx={{ display: "flex", flexDirection: "column", position: "relative" }}
+    >
+      {showOverlay && !isPlaying && (
+        <Box
+          className={isFullScreen ? "overlay-fullscreen" : "overlay"}
+          onClick={toggleOverlay}
+        >
+          <PosterComponent
+            XRayMocks={XRayMocks}
+            currentTime={currentTime}
+            isFullScreen={isFullScreen}
+          />
+        </Box>
+      )}
+      <ReactPlayer
+        url={VIDEO_SRC}
+        width="640px"
+        height="390px"
+        controls
+        playing={isPlaying}
+        onProgress={handleProgress}
+        onPlay={handlePlay}
+        onPause={handlePause}
+        onEnded={handlePause}
       />
+      <Typography>Current Time seconds: {Math.floor(currentTime)}</Typography>
+      <Typography>Current Time minutes: {formatTime(currentTime)}</Typography>
     </Grid>
   );
 }
