dd66ef1c9ed11c47f27966f14268bde365f3205a
AndriusIsin <isin.andrius@gmail.com>
2023-12-05 19:32:36 +0000

    test

--- src/pages/Dashboard/AllUsersPage.js
+++ src/pages/Dashboard/AllUsersPage.js
@@ -1,39 +1,67 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-
+import React, { useEffect, useState } from "react";
+import { getUsers } from "../../api/users";
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+  Typography,
+  Container,
+  Paper,
+} from "@mui/material";
+import { pink } from "@mui/material/colors";
 const AllUsersPage = ({ state }) => {
+  // const { users, isLoading } = useAuth0();
+  const [usersProfileData, setUsersProfileData] = useState("");
+  const user = {
+    name: "Jessica Parker",
+    email: "jessica@example.com",
+  };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     // if (isLoading || !user) {
+  //     //   return;
+  //     // }
+  //     try {
+  //       const userData = await getUsers();
+  //       setUsersProfileData(userData.users);
+  //     } catch (error) {
+  //       console.error({ error });
+  //     }
+  //   };
+  //   fetchData();
+  // }, [isLoading]);
+  // console.log(usersProfileData);
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-start",
-        pt: 2,
-        width: "100%",
-      }}
-    >
-      <Typography
-        variant="h3"
-        sx={{
-          color: "#E6007E",
-          textShadow: "1px 1px 2px rgba(0, 0, 0, 0.3)",
-          animation: "pulse 2s infinite ease-in-out",
-        }}
-      >
-        Coming soon...
-      </Typography>
+    <Container component="main" maxWidth="sm">
+      <CssBaseline />
+      <Paper elevation={3} sx={{ my: 4, p: 3 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: pink[500], width: "auto", height: 100 }}>
+            {user.name.charAt(0)}
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            {user.name}
+          </Typography>
 
-      <style>
-        {`
-          @keyframes pulse {
-            0% { transform: scale(1); }
-            50% { transform: scale(1.1); }
-            100% { transform: scale(1); }
-          }
-        `}
-      </style>
-    </Box>
+          <Button variant="contained" color="error" sx={{ mt: 2 }}>
+            Block account
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
 
