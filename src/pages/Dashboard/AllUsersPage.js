2600f342e354843009707958f8a124ff5a35c36f
AndriusIsin <isin.andrius@gmail.com>
2023-12-05 21:15:19 +0000

    All user page

--- src/pages/Dashboard/AllUsersPage.js
+++ src/pages/Dashboard/AllUsersPage.js
@@ -1,67 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { getUsers } from "../../api/users";
-import { useAuth0 } from "@auth0/auth0-react";
 import {
   Avatar,
   Button,
   CssBaseline,
-  TextField,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
   Box,
+  Grid,
   Typography,
   Container,
   Paper,
+  CircularProgress,
+  Tooltip,
 } from "@mui/material";
-import { pink } from "@mui/material/colors";
 const AllUsersPage = ({ state }) => {
-  // const { users, isLoading } = useAuth0();
   const [usersProfileData, setUsersProfileData] = useState("");
-  const user = {
-    name: "Jessica Parker",
-    email: "jessica@example.com",
-  };
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     // if (isLoading || !user) {
-  //     //   return;
-  //     // }
-  //     try {
-  //       const userData = await getUsers();
-  //       setUsersProfileData(userData.users);
-  //     } catch (error) {
-  //       console.error({ error });
-  //     }
-  //   };
-  //   fetchData();
-  // }, [isLoading]);
-  // console.log(usersProfileData);
-  return (
-    <Container component="main" maxWidth="sm">
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userData = await getUsers();
+        setUsersProfileData(userData.users);
+      } catch (error) {
+        console.error({ error });
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(usersProfileData.users);
+  return usersProfileData ? (
+    <Container component="main">
       <CssBaseline />
-      <Paper elevation={3} sx={{ my: 4, p: 3 }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: pink[500], width: "auto", height: 100 }}>
-            {user.name.charAt(0)}
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            {user.name}
-          </Typography>
-
-          <Button variant="contained" color="error" sx={{ mt: 2 }}>
-            Block account
-          </Button>
-        </Box>
-      </Paper>
+      <Grid container spacing={2} sx={{ mt: 2, justifyContent: "center" }}>
+        {usersProfileData.map((user, index) => (
+          <Grid item xs={12} sm={12} key={index}>
+            <Paper
+              elevation={3}
+              sx={{
+                p: 3,
+                display: "flex",
+                alignItems: "center",
+                maxWidth: "500px",
+                margin: "auto",
+              }}
+            >
+              <Box sx={{ flexShrink: 0, mr: "2rem" }}>
+                <Avatar sx={{ width: 100, height: 100, bgcolor: "#E6007E" }}>
+                  <Typography style={{ color: "white", fontSize: "4rem" }}>
+                    {user.Name.charAt(0)}
+                  </Typography>
+                </Avatar>
+              </Box>
+              <Box sx={{ flexGrow: 1, ml: 2 }}>
+                <Typography component="h2" variant="h4">
+                  {user.Name}
+                </Typography>
+                <Typography variant="h6">{user.Email}</Typography>
+                <Typography variant="h6">Role: {user.Role}</Typography>
+                <Tooltip title="This feature is under development." arrow>
+                  <Button
+                    variant="contained"
+                    sx={{ mt: 2, bgcolor: "#E6007E", color: "white", ml: 20 }}
+                  >
+                    Block account
+                  </Button>
+                </Tooltip>
+              </Box>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
     </Container>
+  ) : (
+    <CircularProgress />
   );
 };
 
