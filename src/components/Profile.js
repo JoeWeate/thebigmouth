5799d00b136ffdfce8c0ab6138fd505b84f54a3e
AndriusIsin <isin.andrius@gmail.com>
2023-12-05 18:48:31 +0000

    adding data from table

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Avatar,
   Button,
@@ -13,10 +13,26 @@ import {
 } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import { useAuth0 } from "@auth0/auth0-react";
+import { getUserById } from "../api/users";
 
 const Profile = () => {
   const { user, isLoading } = useAuth0();
-  console.log(user);
+  const [userProfileData, setUserProfileData] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      if (isLoading || !user) {
+        return;
+      }
+      try {
+        const userData = await getUserById(user.sub);
+        setUserProfileData(userData.user);
+      } catch (error) {
+        console.error({ error });
+      }
+    };
+    fetchData();
+  }, [isLoading]);
+  console.log(userProfileData);
   return (
     <Container component="main" maxWidth="sm">
       <CssBaseline />
@@ -44,7 +60,7 @@ const Profile = () => {
               }}
             ></Avatar>
             <Typography component="h1" variant="h5">
-              {user.name}
+              {userProfileData.Name}
             </Typography>
             <Tooltip title="This feature is under development." arrow>
               <Button variant="contained" component="label" sx={{ mt: 2 }}>
@@ -53,7 +69,7 @@ const Profile = () => {
               </Button>
             </Tooltip>
             <Typography component="h6" sx={{ mt: 2 }}>
-              Role: {user.role}
+              Role: {userProfileData.Role}
             </Typography>
             <Tooltip title="This feature is under development." arrow>
               <Button variant="outlined" startIcon={<Add />} sx={{ mt: 2 }}>
@@ -64,10 +80,10 @@ const Profile = () => {
               margin="normal"
               fullWidth
               id="email"
-              label="Email Address"
+              label=""
               name="email"
               autoComplete="email"
-              value={user.email}
+              value={userProfileData.Email}
               InputProps={{
                 readOnly: true,
               }}
